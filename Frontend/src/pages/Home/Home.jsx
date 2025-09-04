92783dd07bcb5c7b2750d5c28ec08ce77c5aefbd
SwapnilMishra29 <mishraswapnil452@gmail.com>
2025-09-04 23:03:21 +0530

    Update Home.jsx

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import "./home.css"
+import { Suspense } from 'react'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import { useState } from 'react'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/appDownload'
 
 
+// Lazy load FoodDisplay
+const FoodDisplay = React.lazy(()=>
+    import('../../components/FoodDisplay/FoodDisplay')
+  )
+
+ 
+// Skeleton loader component
+const FoodSkeleton = () => {
+  return (
+    <div className="food-skeletons">
+      {[...Array(6)].map((_, i) => (
+        <div key={i} className="skeleton-card"></div>
+      ))}
+    </div>
+  )
+}
+
 
 const Home = () => {
+
+  
     
   const [category,setCategory] = useState("All");
 
@@ -16,7 +35,12 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay category={category}/>
+
+       {/* Wrap FoodDisplay inside Suspense */}
+      <Suspense fallback={<FoodSkeleton/>}>
+           <FoodDisplay category={category}/>
+      </Suspense>
+     
       <AppDownload />
     </div>
   )
